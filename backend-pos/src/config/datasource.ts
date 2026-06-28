0afa0f5a1abd8b49bac96d2b968cbcab218e312f
jear-her <jearwebdeveloper@gmail.com>
2026-06-28 14:03:54 +0700

    update code

--- backend-pos/src/config/datasource.ts
+++ backend-pos/src/config/datasource.ts
@@ -7,7 +7,7 @@ dotenv.config()
 export const AppDataSource= new DataSource({
     type:"postgres",
     host:process.env.DB_HOST,
-    port:parseInt(process.env.DB_PORT||"5543"),
+    port:parseInt(process.env.DB_PORT||"5433"),
     username:process.env.DB_USERNAME,
     password:process.env.DB_PASSWORD,
     database:process.env.DB_DATABASE,
